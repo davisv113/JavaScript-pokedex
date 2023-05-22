faef648c9d23e9593b39da084ef1fff77ae19b31
Davi <davi113@hotmail.com>
2023-05-22 11:52:46 -0300

    alteração do caminho das pastas

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -23,13 +23,13 @@ pokeApi.getPokemonDetail = (pokemon) => {
         .then(convertPokeApiDetailToPokemon)
 }
 
-pokeApi.getPokemons = (offset = 0, limit = 5) => {
+pokeApi.getPokemons = async (offset = 0, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-    return fetch(url)
-        .then((response) => response.json())
-        .then((jsonBody) => jsonBody.results)
-        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
-        .then((detailRequests) => Promise.all(detailRequests))
-        .then((pokemonsDetails) => pokemonsDetails)
+    const response = await fetch(url)
+    const jsonBody = await response.json()
+    const pokemons = jsonBody.results
+    const detailRequests = pokemons.map(pokeApi.getPokemonDetail)
+    const pokemonsDetails = await Promise.all(detailRequests)
+    return pokemonsDetails
 }
